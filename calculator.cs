728ba9f37783bc148cfd8809bdb57969410bf670
aqshalw <aqsalwijaya@students.amikom.ac.id>
2024-03-05 12:10:28 +0700

    menambahkan kode

---
+++ calculator.cs
@@ -0,0 +1,17 @@
+class calculator
+{
+	static void Main(string[] args)
+	{
+		
+	}
+	
+	static int penambahan(int a, int b)
+	{
+		return a + b;
+	}
+}
+static int pengurangan(int a, int b)
+{
+	return a - b;
+}
+	
